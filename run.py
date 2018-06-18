d79e3009cf41cadb93f481586c70ff7257f17f64
Georges Labrèche <georges@opendatakosovo.org>
2018-06-18 19:05:58 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ import argparse
 import json
 from flask import Flask, jsonify, request
 from flask.ext.cors import CORS
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 import os
 
 # Example query strings: 
@@ -16,12 +19,53 @@ import os
 #-------------------------------------------
 app = Flask(__name__)
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+#@app.after_request
+#def after_request(response):
+#  response.headers.add('Access-Control-Allow-Origin', '*')
+#  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#  return response
 
 #cors = CORS(app, resources={r"/output/*": {"origins": "*"}})
 
@@ -44,7 +88,8 @@ def index():
     intro = intro + "</ul>Only the file_name and file_type fields are mandatory.</p>"
     return intro
 
-@app.route('/output', methods=['GET'])
+@app.route('/output', methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def data():
     try:
         file_name = request.args.get('file_name')
